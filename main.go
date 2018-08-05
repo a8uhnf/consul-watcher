584f051c2453375b399bbcdfbb40433df36778f2
a8uhnf <a8u.han1fa@gmail.com>
2018-08-06 00:35:43 +0600

    added-all

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"net/http"
+	"github.com/hashicorp/consul/api"
+"fmt"
 )
 
 func main() {
@@ -20,4 +22,31 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(rq)
+}
+
+
+
+func consul_test_api() {
+	// Get a new client
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	// Get a handle to the KV API
+	kv := client.KV()
+
+	// PUT a new KV pair
+	p := &api.KVPair{Key: "REDIS_MAXCLIENTS", Value: []byte("1000")}
+	_, err = kv.Put(p, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Lookup the pair
+	pair, _, err := kv.Get("REDIS_MAXCLIENTS", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("KV: %v %s\n", pair.Key, pair.Value)
 }
