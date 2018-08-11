b03d0befb40671cbeb610f0c7e67b3e8684967d2
hanif <abuhanifa@pathao.com>
2018-08-12 00:02:22 +0600

    added-all

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -13,6 +15,10 @@ import (
 	"github.com/spf13/viper"
 	_ "github.com/spf13/viper/remote"
 )
+
+type HelloStruct struct {
+	Good string `json:"good"`
+}
 
 func main() {
 	fmt.Println("Hello World!!!")
@@ -33,10 +39,12 @@ func main() {
 	}
 
 	fmt.Println(string(b))
-
+	stop := make(chan int)
 	// consul_test_api()
 	// watchConsulConfig()
-	runServer()
+	go runServer()
+	watchExample()
+	<-stop
 }
 
 func runServer() {
@@ -49,8 +57,41 @@ func runServer() {
 	}
 }
 
-type HelloStruct struct {
-	Good string `json:"good"`
+func watchExample() {
+	m := make(map[string]interface{})
+	m["datacenter"] = "dc1"
+	m["type"] = "key"
+	m["key"] = "/hello"
+	m["handler_type"] = "http"
+	m["http_handler_config"] = makeParams(`{"path":"http://localhost/hello","method":"GET","header":{},"timeout":"10s","tls_skip_verify":true}`)
+
+	plan, err := watch.Parse(m)
+	if err != nil {
+		panic(err)
+	}
+	plan.Watcher = func(pp *watch.Plan) (watch.BlockingParamVal, interface{}, error) {
+		fmt.Println("Hello World!!!", pp)
+		t := &HelloBlockingParamVal{}
+		st := &HelloStruct{}
+		return t, st, nil
+	}
+	err = plan.Run(fmt.Sprintf("http://%s%s", "localhost", ":8500"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+type HelloBlockingParamVal struct{}
+
+func (hbpv *HelloBlockingParamVal) Equal(other watch.BlockingParamVal) bool {
+	fmt.Println("Hello World equal!!!", other)
+	panic("hello")
+	return true
+}
+func (hbpv *HelloBlockingParamVal) Next(previous watch.BlockingParamVal) watch.BlockingParamVal {
+	fmt.Println("Hello World next!!!", previous)
+	// time.Sleep(10 * time.Second)
+	return previous
 }
 
 func watchConsulConfig() {
@@ -119,4 +160,13 @@ func consul_test_api() {
 		panic(err)
 	}
 	fmt.Printf("KV: %v %s\n", pair.Key, pair.Value)
+}
+
+func makeParams(s string) map[string]interface{} {
+	var out map[string]interface{}
+	dec := json.NewDecoder(bytes.NewReader([]byte(s)))
+	if err := dec.Decode(&out); err != nil {
+		panic(err)
+	}
+	return out
 }
